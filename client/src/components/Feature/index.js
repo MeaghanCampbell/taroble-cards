9cbc10b130155b6ad9d4ff2322fb49ffc7e7de2e
J Scott Rumptz <jscott@rumptz.tech>
2021-05-17 20:38:21 -0500

    Removed Tailwindcss

--- client/src/components/Feature/index.js
+++ client/src/components/Feature/index.js
@@ -8,13 +8,13 @@ const stats = [
 
 export default function Feature() {
     return (
-        <div className="bg-gray-300 p-10">
-            <h3 className="text-lg leading-6 font-medium text-gray-900">Last 30 days</h3>
-            <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+        <div>
+            <h3>Last 30 days</h3>
+            <dl>
                 {stats.map((item) => (
-                    <div key={item.name} className="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
-                        <dt className="text-sm font-medium text-gray-500 truncate">{item.name}</dt>
-                        <dd className="mt-1 text-3xl font-semibold text-gray-900">{item.stat}</dd>
+                    <div key={item.name}>
+                        <dt>{item.name}</dt>
+                        <dd>{item.stat}</dd>
                     </div>
                 ))}
             </dl>
