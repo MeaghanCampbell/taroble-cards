34be0916c0159cebd95e0fa725fa5ad8e3efbc3f
Count Zero <joelecox1@gmail.com>
2021-05-18 20:26:34 -0500

    login & sign up styles for iphone8

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
 const Home = () => {
-    return (
-        <div>
-            <div className='container'>
-                <h1 className='title-home'>Taroble Cards</h1>
-            </div>
-            <div className='container'>
-                    <button type="submit">GET A READING</button>
-                </div>
-            </div>
-    )
+	return (
+		<div>
+			<div className='container'>
+				<h1 className='title-home'>Taroble Cards</h1>
+			</div>
+			<div className='container'>
+				<button type="submit">GET A READING</button>
+			</div>
+		</div>
+	)
 }
 
-export default Home
+export default Home;
--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -6,84 +6,81 @@ import { LOGIN_USER } from '../../utils/mutations';
 import Auth from '../../utils/auth';
 
 const Login = (props) => {
-    const [formState, setFormState] = useState({ email: '', password: '' });
+	const [formState, setFormState] = useState({ email: '', password: '' });
 
-    const [login, { error }] = useMutation(LOGIN_USER);
+	const [login, { error }] = useMutation(LOGIN_USER);
 
-    // update state based on form input changes
-    const handleChange = (event) => {
-        const { name, value } = event.target;
+	// update state based on form input changes
+	const handleChange = (event) => {
+		const { name, value } = event.target;
 
-        setFormState({
-            ...formState,
-            [name]: value,
-        });
-    };
+		setFormState({
+			...formState,
+			[name]: value,
+		});
+	};
 
-    // submit form
-    const handleFormSubmit = async event => {
-        event.preventDefault();
+	// submit form
+	const handleFormSubmit = async event => {
+		event.preventDefault();
 
-        try {
-            const { data } = await login({
-                variables: { ...formState }
-            });
-            // put token in local storage
-            Auth.login(data.login.token);
-        } catch (e) {
-            console.error(e);
-        }
-    };
-    return (
-        <div>
-                    <form onSubmit={handleFormSubmit}>
-                        <div>
-                            <label htmlFor="email">
-                                Email address
-                            </label>
-                            <div>
-                                <input
-                                    id="email"
-                                    name="email"
-                                    type="email"
-                                    placeholder="email"
-                                    autoComplete="email"
-                                    required
-                                    value={formState.email}
-                                    onChange={handleChange}
-                                />
-                            </div>
-                        </div>
+		try {
+			const { data } = await login({
+				variables: { ...formState }
+			});
+			// put token in local storage
+			Auth.login(data.login.token);
+		} catch (e) {
+			console.error(e);
+		}
+	};
+	return (
+		<div>
+			<form className='login-signup-form' onSubmit={handleFormSubmit}>
+				<div>
+					<p className='form-header'>LOGIN </p>
+					<p className='form-header'>TO SAVE YOUR</p>
+					<p className='form-header'>READINGS</p>
+				</div>
+				<div>
+					<div>
+						<input
+							id="email"
+							name="email"
+							type="email"
+							placeholder="email"
+							autoComplete="email"
+							required
+							value={formState.email}
+							onChange={handleChange}
+						/>
+					</div>
+				</div>
 
-                        <div>
-                            <label htmlFor="password">
-                                password
-                            </label>
-                            <div>
-                                <input
-                                    id="password"
-                                    name="password"
-                                    type="password"
-                                    placeholder='******'
-                                    autoComplete="current-password"
-                                    value={formState.password}
-                                    onChange={handleChange}
-                                    required
-                                />
-                            </div>
-                        </div>
+				<div>
+					<div>
+						<input
+							id="password"
+							name="password"
+							type="password"
+							placeholder='password'
+							autoComplete="current-password"
+							value={formState.password}
+							onChange={handleChange}
+							required
+						/>
+					</div>
+				</div>
 
-                        <div>
-                            <button
-                                type="submit"
-                            >
-                                Sign in
-                            </button>
-                        </div>
-                    </form>
-                    {error && <div>Login failed</div>}
-                </div>
-    )
+				<div>
+					<button className='sign-in-btn' type="submit">
+						LOGIN
+          </button>
+				</div>
+			</form>
+			{error && <div>login failed.</div>}
+		</div>
+	)
 }
 
 export default Login;
