a60ea0d9a05122318dfa3f67202c2d2aabb8ac60
J Scott Rumptz <jscott@rumptz.tech>
2021-05-19 20:48:31 -0500

    database name change

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // enter desired <remote env variable name> || <local database name> here
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/user-login', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/taroble-cards', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
